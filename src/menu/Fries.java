e0203cf747befa8cb7ae1964f8b28969e4eed1da
Ryan Oates <ryanoatsie@outlook.com>
2024-11-07 19:28:55 -0800

    update

--- src/menu/Fries.java
+++ src/menu/Fries.java
@@ -7,11 +7,6 @@ public class Fries extends MenuItem {
    public Fries(Long id, String name, String description, double basePrice, Size size) {
       super(id, name, description, basePrice, "FRIES", 5); // 5 minutes prep time
       this.size = size;
-      this.seasoned = false;
-   }
-
-   public void addSeasoning() {
-      this.seasoned = true;
    }
 
    public Size getSize() {
--- src/menu/Hamburger.java
+++ src/menu/Hamburger.java
@@ -1,24 +1,12 @@
 package menu;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Hamburger extends MenuItem {
-   private List<Topping> toppings;
-
    public Hamburger(Long id, String name, String description, double basePrice) {
       super(id, name, description, basePrice, "HAMBURGER", 10); // 10 minutes prep time
-      this.toppings = new ArrayList<>();
-   }
-
-   public void addTopping(Topping topping) {
-      toppings.add(topping);
    }
 
    @Override
    public double calculateTotal() {
-      return getPrice() + toppings.stream()
-            .mapToDouble(Topping::getPrice)
-            .sum();
+      return getPrice();
    }
 }
