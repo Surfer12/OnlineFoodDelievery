4d2d71b06eda29a92f4fee4ddb03a572f440ea36
Surfer12 <40179506+Surfer12@users.noreply.github.com>
2024-11-25 11:25:51 -0800

    Define custom build tasks and configure build tasks in `build.gradle`
    * Add plugins for checkstyle, pmd, and jacoco
    * Add dependencies for logging libraries and code quality tools
    * Configure checkstyle, pmd, and jacoco
    * Add tasks for code formatting, generating documentation, and packaging into JAR and WAR files
    * Configure build tasks to include code coverage reports using JaCoCo
    
    Remove redundant imports in `src/test/java/app/Application.java`

--- src/test/java/NotificationServiceTest.java
+++ src/test/java/NotificationServiceTest.java
@@ -4,7 +4,6 @@ import orderUtilities.OrderStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import notification.NotificationService;
 
 import static org.mockito.Mockito.*;
 
