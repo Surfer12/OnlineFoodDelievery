0828681c5574aad2a09932c1940207ee0898d03d
Ryan Oates <ryanoatsie@outlook.com>
2024-12-01 03:34:16 -0800

    Remove deprecated Gradle configuration and notification service implementations

--- OnlineFoodDelievery/src/main/java/notification/BasicNotificationService.java
+++
@@ -1,8 +0,0 @@
-package notification;
-
-public class BasicNotificationService implements NotificationService {
-    @Override
-    public void sendNotification(String recipient, String message) {
-        System.out.printf("Notification to %s: %s%n", recipient, message);
-    }
-} 
