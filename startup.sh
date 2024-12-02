4742651ab93402bde6a115e5ac4b502c1e5d3517
Ryan Oates <ryanoatsie@outlook.com>
2024-12-01 18:08:47 -0800

    Upgrade Spring Boot version to 3.4.0 in build.gradle and remove obsolete startup.sh script. Update Eclipse build settings to enable auto-sync and set the correct Java home path. Modify Gradle cache files to reflect the current project state.

--- startup.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-# ...existing startup commands...
-java -jar OnlineFoodDelieveryV.01.jar
-# Remove or comment out the heap dump option
-# java -XX:+HeapDumpOnOutOfMemoryError -jar OnlineFoodDelieveryV.01.jar
